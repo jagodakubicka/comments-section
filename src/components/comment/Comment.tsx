fb7ad5bb732501a19d098dfaabd183f984c5dc03
Jagoda Kubicka <93054999+jagodakubicka@users.noreply.github.com>
2024-05-19 11:56:20 +0200

    display buttons if author of comment

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,21 +1,44 @@
+import { useState } from 'react';
 import { CommentProps } from '../comments/Comments';
 import './styles.css';
 
 export const Comment: React.FC<CommentProps> = (data) => {
+  const [likes, setLikes] = useState(0);
+  const [canEdit, setCanEdit] = useState(false);
+
   return (
     <div className='comment'>
       <div className='comment-top'>
         <p className='comment-user-nickname'>{data.user}</p>
-        <button type='button' className='comment-likes'>
-          <span>O</span>
+        <button
+          type='button'
+          className='comment-likes'
+          onClick={() => setLikes(likes + 1)}>
+          <span>Likes: </span>
+          <span>{likes}</span>
         </button>
       </div>
       <div className='comment-content'>
-        <textarea className='comment-text' disabled value={data.desc} />
+        <textarea
+          className='comment-text'
+          disabled={!canEdit ? true : false}
+          value={data.desc}
+        />
       </div>
       <div className='comment-bottom'>
-        <button className='comment-btn comment-delete'>delete</button>
-        <button className='comment-btn comment-edit'>edit</button>
+        {data.user === 'You' ? (
+          <>
+            <button className='comment-btn comment-delete'>delete</button>
+            <button
+              className='comment-btn comment-edit'
+              onClick={() => setCanEdit(!canEdit)}>
+              edit
+            </button>
+          </>
+        ) : (
+          ''
+        )}
+
         <button className='comment-btn comment-reply'>reply</button>
       </div>
     </div>
